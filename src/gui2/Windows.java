bb220548f48d1e53a11b1f69e76077d65b554b85
alexolivaresdv <hp.alex.ao@gmail.com>
2017-11-08 21:09:50 -0300

    Se crean todos los contructores y objetos

--- src/gui2/Windows.java
+++ src/gui2/Windows.java
@@ -4,11 +4,34 @@
  * and open the template in the editor.
  */
 package gui2;
+import java.awt.Color;
+import javax.swing.*;
 
 /**
  *
  * @author LAB17
  */
-public class Windows {
+public class Windows extends JFrame
+{
+    JPanel panel;
+    JLabel label;
+    JTextField texto; 
+    JCheckBox vereficador;
+    JRadioButton seleccion;
+    JButton boton;
     
+    public Windows()
+    {
+        panel = new JPanel();
+        label = new JLabel();
+        texto = new JTextField(25);
+        vereficador = new JCheckBox();
+        seleccion = new JRadioButton();
+        boton = new JButton();
+        
+        label.setText("Nombre");
+        boton.setText("Enviar");
+        panel.setBackground(Color.BLUE);
+        
+    }
 }
